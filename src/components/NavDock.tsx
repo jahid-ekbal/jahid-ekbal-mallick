46771d51ca0891c5e2fc54ef83bcfedd19182ce4
Jahid Ekbal Mallick <jahidekbalmallick@gmail.com>
2026-05-17 05:24:17 +0530

    Add PR icon to NavDock and Edge launch config
    Import and render GitPullRequestCreateIcon in NavDock for a new dock item. Add VS Code Edge launch configurations (.vscode/launch.json) for launching and attaching DevTools. Also add @react-icons/all-files to package.json as a new dependency.

--- src/components/NavDock.tsx
+++ src/components/NavDock.tsx
@@ -1,13 +1,19 @@
-import { HomeIcon, SettingsIcon } from "lucide-react";
+import { GitPullRequestCreateIcon, HomeIcon, SettingsIcon } from "lucide-react";
+
 import { AnimatedThemeToggler } from "./shadcnui/animated-theme-toggler";
 import { Dock, DockIcon } from "./shadcnui/dock";
-
 const NavDock = () => {
   return (
     <div className="fixed bottom-6 left-1/2 z-50 -translate-x-1/2">
       <Dock>
         <DockIcon>
           <HomeIcon />
+        </DockIcon>
+        <DockIcon>
+          <GitPullRequestCreateIcon />
+        </DockIcon>
+        <DockIcon>
+          <SettingsIcon />
         </DockIcon>
         <DockIcon>
           <SettingsIcon />
